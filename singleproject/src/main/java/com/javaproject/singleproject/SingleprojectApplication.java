bf765d81b460e61a0e2594117417700fb5b62339
dkqpqp2 <dkqpqp2@gmail.com>
2025-02-11 10:23:28 +0900

    Frist Commit

---
+++ singleproject/src/main/java/com/javaproject/singleproject/SingleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.singleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleprojectApplication.class, args);
+	}
+
+}
